e225e36589b965e9882ea088c98b0467b5b094d8
Dipesh Patel <dipesh.patel@bbc.co.uk>
2012-04-30 17:31:29 +0100

    Added mocks for testing via sinon

--- test/Driver/Psql.js
+++ test/Driver/Psql.js
@@ -1,10 +1,28 @@
 var should = require('should');
 var Driver = require('../../lib/Driver/Psql');
+var sinon = require('sinon');
 
 suite('Psql', function(){
-  test('should be an instance of Psql', function(){
+  var mockPg;
+  setup(function(){
+    mockPg = sinon.mock(require('pg').native);
+  });
+  teardown(function(){
+    mockPg.restore();
+  });
+  test('#new()', function(){
     var driver = new Driver();
     driver.should.be.an.instanceof(Psql);
+  });
+  test('#apply()', function(done){
+    mockPg.expects('connect').yields(null, {
+      query: function (sql, callback) {
+        sql.should.match(/^BEGIN|TEST|COMMIT;$/);
+        callback();
+      }
+    });
+    var driver = new Driver();
+    driver.apply("TEST;",done);
   });
 });
 
--- test/Migration.js
+++ test/Migration.js
@@ -2,7 +2,7 @@ var should = require('should');
 var Migration = require('../lib/Migration');
 
 suite('Migration', function(){
-  test('instantiated with three args', function(){
+  test('#new(dir,title,date)', function(){
     var date = new Date();
     var mig = new Migration('foo','bar', date);
     mig.should.be.an.instanceof(Migration);
@@ -12,7 +12,7 @@ suite('Migration', function(){
     mig.down_path.should.equal('foo/down/'+date.getTime()+'-bar.sql');
   });
 
-  test('instantiated with two args', function(){
+  test('#new(dir,title)', function(){
     var date = new Date();
     var mig = new Migration('foo', date.getTime()+'-bar.sql');
     mig.should.be.an.instanceof(Migration);
